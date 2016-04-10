8fe0dae8e155111977dba87f622e63c62a8b867d
Huang Chao <huangchao0825@gmail.com>
2016-04-10 09:23:57 +0800

    Init Project

---
+++ Heuristic.java
@@ -0,0 +1,18 @@
+public class Heuristic {
+
+	/* heuristic function, given a state and one (potential) move, 
+	return the weighted heurtisticValue combining each feature */
+	public static double heuristicValue(State s, int[] move) {
+		return 0;		
+	}
+
+	// gonna to decide the weights for each feature and set them as constant values
+	// one example below
+	private static final double WEIGHT_ELIMATED_ROWS = 1;
+
+	// gonna to write a separate heuristic function for every feature
+	// one example below
+	private static double valueForFeatureEliminatedRows(State s, int[] move) {
+		return 0;
+	}
+}
